1ed040dcc52900c2b41905b64b3bea706b4870c7
Abdiaziz Ahmed <84683112+abdiaziz2112@users.noreply.github.com>
2022-07-10 19:54:12 +0300

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,71 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+
+   
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E001",
+    src: "E001"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E002",
+    src: "E002"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E003",
+    src: "E003"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E004",
+    src: "E004"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E005",
+    src: "E005"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E006",
+    src: "E006"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E006",
+    src: "E006"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E007",
+    src: "E007"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E008",
+    src: "E008"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E009",
+    src: "E009"
+  },
+  {
+    name: "Teacher Farah Hirsi",
+    artist: "Lesson E010",
+    src: "E010"
+  },
+
+];
